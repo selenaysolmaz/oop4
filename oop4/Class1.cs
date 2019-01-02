8a3e4f324b661fcc98e2aa1ca8e8c9699e0a1e25
semvs <semvs@ayvansaray.edu.tr>
2019-01-02 11:06:32 +0300

    Add project files.

---
+++ oop4/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop4
+{
+    public class Yazici
+    {
+        public void Yazdir (int i)
+        {
+            Console.WriteLine("sayisal degerler yazdiriliyor - {0}", i);
+        }
+        public void Yazdir(string s)
+        {
+            Console.WriteLine("metinsel değerler yazdırılıyor-{0}", s);
+        }
+    }
+}
---
+++ oop4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//çok biçimlilik örnek
+namespace oop4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Yazici yazici = new Yazici();
+            yazici.Yazdir("ayvansaray");
+            yazici.Yazdir(2019);
+            Console.WriteLine("işlem tamamlandı.");
+            Console.ReadKey();
+
+        }
+    }
+}
